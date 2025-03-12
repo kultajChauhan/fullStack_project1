6a6f9a53d9e316b3bc506c8c5579cdc1c6a44f22
kultajSInghChauhan <kultajchauhan121@gmail.com>
2025-03-12 17:38:08 +0530

    user model done

---
+++ model/User.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+let userSchema = new mongoose.Schema(
+  {
+    name: String,
+    password: String,
+    email: String,
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+    },
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpires: {
+      type: Date(),
+    },
+  },
+  { timestamps: true }
+);
+
+let User = mongoose.model("User", userSchema);
+
+export default User;
